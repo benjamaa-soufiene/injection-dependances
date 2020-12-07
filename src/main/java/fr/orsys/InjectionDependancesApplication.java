cf14bb9b433a8db912521edde92b75346e28fd4d
BEN JAMAA Soufiene <benjamaasoufiene@gmail.com>
2020-12-07 11:33:57 +0100

    Formation Orsys : Exercice Injection de des pendances

---
+++ src/main/java/fr/orsys/InjectionDependancesApplication.java
@@ -0,0 +1,24 @@
+package fr.orsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class InjectionDependancesApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(InjectionDependancesApplication.class, args);
+		Boite b1 = (Boite) ctx.getBean(Boite.class);
+		b1.setNbRapports(5);
+		Moteur m1 =(Moteur) ctx.getBean(Moteur.class);
+	//	m1.setBoite(b1);
+		m1.setPuissances(60);
+		Voiture v1 =(Voiture) ctx.getBean(Voiture.class);
+		v1.setNom("Peugeot 205");
+	//	v1.setMoteur(m1);
+		System.out.println("La voiture " + v1.getNom() + " posséde une puissance de " + v1.getMoteur().getPuissances()
+				+ " chevaux, le NbRapports est "+v1.getMoteur().getBoite().getNbRapports()+" et sa consommation est de " + v1.getMoteur().getConsommation() + "%.");
+	}
+
+}
